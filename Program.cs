21fe920871e01f848f18083b97c519d841133d87
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2022-06-27 10:30:21 -0300

    log do BaseAddress

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 // builder.RootComponents.Add<HeadOutlet>("head::after");
 
+Console.WriteLine("Program.cs:\n\tbuilder.HostEnvironment.BaseAddress={0}", builder.HostEnvironment.BaseAddress);
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 // builder.Services.AddSingleton<DemoService>();
 builder.Services.AddScoped<DemoService>();
