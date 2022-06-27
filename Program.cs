36ccd6e8840481e590a91ff7da2e029011970771
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2022-06-27 11:12:28 -0300

    log do ambiente

--- Program.cs
+++ Program.cs
@@ -7,7 +7,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 // builder.RootComponents.Add<HeadOutlet>("head::after");
 
-Console.WriteLine("Program.cs:\n\tbuilder.HostEnvironment.BaseAddress={0}", builder.HostEnvironment.BaseAddress);
+// https://docs.microsoft.com/pt-br/aspnet/core/blazor/fundamentals/configuration?view=aspnetcore-6.0
+
+Console.WriteLine("Program.cs:");
+Console.WriteLine("\tbuilder.HostEnvironment.BaseAddress={0}", builder.HostEnvironment.BaseAddress);
+Console.WriteLine("\tbuilder.HostEnvironment.Environment={0}", builder.HostEnvironment.Environment);
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 // builder.Services.AddSingleton<DemoService>();
