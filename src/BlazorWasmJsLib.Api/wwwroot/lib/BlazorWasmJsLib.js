bb5c891a529893c6d636574b0f2687044122280f
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2022-06-28 13:54:06 -0300

    pegando BLAZOR_WASM_JS_LIB_BASE_URL da window

--- src/BlazorWasmJsLib.Api/wwwroot/lib/BlazorWasmJsLib.js
+++ src/BlazorWasmJsLib.Api/wwwroot/lib/BlazorWasmJsLib.js
@@ -16,13 +16,21 @@ class BlazorWasmJsLib {
     // https://docs.microsoft.com/pt-br/aspnet/core/blazor/fundamentals/environments?view=aspnetcore-6.0#set-the-environment-via-startup-configuration
     console.debug("beforeStart");
     console.time("blazor start time");
+    if ("BLAZOR_WASM_JS_LIB_BASE_URL" in window) {
+      console.debug("BLAZOR_WASM_JS_LIB_BASE_URL:", window.BLAZOR_WASM_JS_LIB_BASE_URL);
+    } else {
+      console.debug("BLAZOR_WASM_JS_LIB_BASE_URL não definida");
+    }
+
     Blazor.start({
       // environment: "Staging",
       // environment: "Development", // window.location.hostname.includes("localhost")
       // environment: "Production",
       loadBootResource: function (type, name, defaultUri, integrity) {
         console.debug(`Loading: '${type}', '${name}', '${defaultUri}'`);
-        // return `blib/_framework/${name}`;
+        if ("BLAZOR_WASM_JS_LIB_BASE_URL" in window) {
+          return `${window.BLAZOR_WASM_JS_LIB_BASE_URL}/_framework/${name}`;
+        }
         return defaultUri;
       }
     })
