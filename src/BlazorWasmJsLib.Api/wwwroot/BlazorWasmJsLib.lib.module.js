c86e3ae6e441e2ff5cc6285a96e11bb967b10086
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2022-06-27 23:49:22 -0300

    reduz configurações do projeto

--- src/BlazorWasmJsLib.Api/wwwroot/BlazorWasmJsLib.lib.module.js
+++
@@ -1,7 +0,0 @@
-export function beforeStart(options, extensions) {
-    console.log("beforeStart", options, extensions);
-}
-
-export function afterStarted(blazor) {
-    console.log("afterStarted", blazor);
-}
--- src/BlazorWasmJsLib.Api/wwwroot/lib/BlazorWasmJsLib.js
+++ src/BlazorWasmJsLib.Api/wwwroot/lib/BlazorWasmJsLib.js
@@ -10,17 +10,21 @@ class BlazorWasmJsLib {
     this._blazorStartedProm = new Promise(resolve => {
       this._resolveBlazorStartedProm = resolve;
     });
+
+    // https://docs.microsoft.com/pt-br/aspnet/core/blazor/fundamentals/startup?view=aspnetcore-6.0#load-boot-resources
     // https://docs.microsoft.com/pt-br/aspnet/core/blazor/fundamentals/configuration?view=aspnetcore-6.0
     // https://docs.microsoft.com/pt-br/aspnet/core/blazor/fundamentals/environments?view=aspnetcore-6.0#set-the-environment-via-startup-configuration
-    // if (window.location.hostname.includes("localhost")) {
-    //   Blazor.start({
-    //     environment: "Staging"
-    //   });
-    // } else {
-    //   Blazor.start({
-    //     environment: "Production"
-    //   });
-    // }
+    console.debug("beforeStart");
+    Blazor.start({
+      // environment: "Staging",
+      // environment: "Development", // window.location.hostname.includes("localhost")
+      // environment: "Production",
+      // loadBootResource: function (type, name, defaultUri, integrity) {
+      //   console.debug(`Loading: '${type}', '${name}', '${defaultUri}'`);
+      //   return `blib/_framework/${name}`;
+      // }
+    })
+      .then(() => console.debug("afterStarted, blazor:", Blazor))
   }
 
   _blazorStarted(dotNetHelper) {
